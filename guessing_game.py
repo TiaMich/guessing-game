bcae982751de3636ff82938a5029cc8f8a523f4c
Tia Moore <148154970+TiaMich@users.noreply.github.com>
2023-10-26 01:25:03 -0500

    Not quite there: Included Errors
    The error messaging is now present in the code, however it is not working properly. Need to correct this!

--- guessing_game.py
+++ guessing_game.py
@@ -6,57 +6,76 @@ Project 1 - A Number Guessing Game
 import random
 from statistics import mean, mode, median
 
-solution = random.randint(1,10)
 attempt_list = []
 
-# create a function for the game, help, and play again function
+# I have no specific reason for why I made these two variables,
+# other than trying to clean up the look of start game function.
+low_guess = "Too low, try again."
+high_guess = "Too high, try again."
+
+#the menu options prompt
 def menu():
     print("+++ Number Guessing Game Menu +++")
-    print("""
+    print("""Options:
     Enter 'HELP' for game instructions
     Enter 'PLAY' to begin the game
     Enter 'EXIT' to exit the program
     """)
+
+#game stats following completion of the game
 def stats():
-    print(f"[Highscore: {min(attempt_list)}]")
-    print(f"""
+    print(f"""[Best score: {min(attempt_list)}]
+    This game took {attempt_list[-1]} attempts.
     The average of all scores is {mean(attempt_list)}.
     The median of all scores is {median(attempt_list)}.
     The mode of all scores is {mode(attempt_list)}.
     """)
-    
-def show_help():
-    print("**Guess a number between 1 and 10. Your lowest number of attempts is your highscore!**")
 
+# start menu option    
+def show_help():
+    print("[Instructions: Guess a number between 1 and 10. Your lowest number of attempts is your highscore!]")
+
+# game function, errors and exceptions
 def start_game():
     play_again = 'y'
     while play_again.lower() == 'y':
         solution = random.randint(1,10)
         print("***** Welcome to the Number Guessing Game! *****")
-        user_guess = int(input("Enter your guess: "))
+        user_guess = input("Enter your guess: ")
         tries = 1
-        while user_guess != solution:
-            print(f"[Total attempts: {tries}]") #new
-            
-            if user_guess < solution:
-                tries += 1
-                user_guess = int(input("Too low. Try again: "))
-            elif user_guess > solution:
-                tries += 1
-                user_guess = int(input("Too high. Try again: "))
-        else:
-            print("Got it! Great job.")
-            attempt_list.append(tries) #new
-            print(f"[Your score: {tries}]")
-            stats()
-            play_again = input("Would you like to play again? (y/n): ")
+        try:
+            user_guess = int(user_guess)
+            if user_guess > 10:
+                raise ValueError()
+                continue
+            elif user_guess < 1:
+                raise ValueError()
+                continue
+            while user_guess != solution:
+                print(f"[Attempt: {tries}]")
+                if int(user_guess) < solution:
+                    tries += 1
+                    print(low_guess)
+                    user_guess = int(input("Your guess: "))
+                elif int(user_guess) > solution:
+                    tries += 1
+                    print (high_guess)
+                    user_guess = int(input("Your guess: "))
+            else:
+                print("Got it! Great job.")
+                attempt_list.append(tries)
+                print(f"[Your score: {tries}]")
+                stats()
+        except ValueError:
+            print("HEY! That's not a valid integer. Please try again.")
+            continue
+        play_again = input("Would you like to play again? (y/n): ")
     else:
         print("Thanks for playing! Here are your finishing stats:")
         print(f"[Your score: {tries}]")
         stats()
-        
-    menu()
-    
+
+# Lastly, calling the code to run in this order:
 menu()
 while True:
     user_choice = input("What would you like to do? -> ")
@@ -66,18 +85,10 @@ while True:
         show_help()
         continue
     elif user_choice.lower() == 'play':
-        play_again = 'y'
         start_game()
         break
 
 
-#   5. Display the following data to the player
-#     a. How many attempts it took them to get the correct number in this game
-#     b. The mean of the saved attempts list
-#     c. The median of the saved attempts list
-#     d. The mode of the saved attempts list
-#   6. Prompt the player to play again
-#     a. If they decide to play again, start the game loop over.
-#     b. If they decide to quit, show them a goodbye message.
-# ( You can add more features/enhancements if you'd like to. )
-# Kick off the program by calling the start_game function.
+# Trying to work out some error issues: 
+# 1) Why are some of my errors are passive? For example: if I guess an integer within range on my first attempt everything works fine. But say I guess an out-of-range integer for my second attempt, it will not raise an error (FYI: if i guess out-of-range on my first attempt, it WILL raise an error). I'm sure there is some issue in my order of things.
+# 2) my raised errors will restart the game, re-randomizing the number. How do I put it so that it continues to let the user try again within the current game?
