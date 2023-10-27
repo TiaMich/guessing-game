841a4b6dc43babaeeb5965f7480e2d4979c15ba3
Tia Moore <148154970+TiaMich@users.noreply.github.com>
2023-10-27 17:57:41 -0500

    Finished...almost
    My ValueError prompts "need some milk," in other words, they are not outputting in the way that I'd like them to.

--- guessing_game.py
+++ guessing_game.py
@@ -8,77 +8,86 @@ from statistics import mean, mode, median
 
 attempt_list = []
 
-# I have no specific reason for why I made these two variables,
-# other than trying to clean up the look of start game function.
-low_guess = "Too low, try again."
-high_guess = "Too high, try again."
-
 #the menu options prompt
 def menu():
-    print("+++ Number Guessing Game Menu +++")
-    print("""Options:
-    Enter 'HELP' for game instructions
-    Enter 'PLAY' to begin the game
-    Enter 'EXIT' to exit the program
+    print("***** WELCOME TO THE NUMBER GUESSING GAME *****")
+    print("""
+    MENU:
+        Enter 'HELP' for game instructions
+        Enter 'PLAY' to begin the game
+        Enter 'EXIT' to exit the program
     """)
 
 #game stats following completion of the game
 def stats():
-    print(f"""[Best score: {min(attempt_list)}]
-    This game took {attempt_list[-1]} attempts.
-    The average of all scores is {mean(attempt_list)}.
-    The median of all scores is {median(attempt_list)}.
-    The mode of all scores is {mode(attempt_list)}.
+    print(f"""STATS:
+    [Best attempt score: {min(attempt_list)}]
+    * Your last game used {attempt_list[-1]} attempts.
+    * The average of all game scores is {mean(attempt_list)}.
+    * The median of all game scores is {median(attempt_list)}.
+    * The mode of all game scores is {mode(attempt_list)}.
     """)
 
 # start menu option    
 def show_help():
-    print("[Instructions: Guess a number between 1 and 10. Your lowest number of attempts is your highscore!]")
+    print("""
+    Instructions:
+        Guess a number between 1 and 10.
+        Try to get the lowest number of attempts possible!
+    """)
 
-# game function, errors and exceptions
+# game function
 def start_game():
-    play_again = 'y'
-    while play_again.lower() == 'y':
-        solution = random.randint(1,10)
-        print("***** Welcome to the Number Guessing Game! *****")
-        user_guess = input("Enter your guess: ")
-        tries = 1
+    tries = 0
+    solution = random.randint(1,10)
+    print("++++ Ready, Set, Go! ++++")
+    print("solution = ", solution) # DELETE THIS LINE
+    while True:
         try:
-            user_guess = int(user_guess)
-            if user_guess > 10:
-                raise ValueError()
+            user_guess = input("Enter your guess: ")
+            if int(user_guess) > 10:
+                tries += 1
+                raise ValueError("OOPS!: Please use an integer between 1 and 10.")
                 continue
-            elif user_guess < 1:
-                raise ValueError()
+            elif int(user_guess) < 1:
+                tries += 1
+                raise ValueError("OOPS!: Please use an integer between 1 and 10.")
                 continue
-            while user_guess != solution:
-                print(f"[Attempt: {tries}]")
-                if int(user_guess) < solution:
-                    tries += 1
-                    print(low_guess)
-                    user_guess = int(input("Your guess: "))
-                elif int(user_guess) > solution:
-                    tries += 1
-                    print (high_guess)
-                    user_guess = int(input("Your guess: "))
+            elif int(user_guess) < solution:
+                tries += 1
+                print("Too low, try again.")
+                continue
+            elif int(user_guess) > solution:
+                tries += 1
+                print ("Too high, try again.")
+                continue
             else:
+                tries += 1
                 print("Got it! Great job.")
                 attempt_list.append(tries)
-                print(f"[Your score: {tries}]")
-                stats()
-        except ValueError:
-            print("HEY! That's not a valid integer. Please try again.")
+                print(f"[Your attempt score: {tries}]")
+        except ValueError as err:
+            print("ERROR: That's not an integer. Please try again.")
             continue
-        play_again = input("Would you like to play again? (y/n): ")
-    else:
-        print("Thanks for playing! Here are your finishing stats:")
-        print(f"[Your score: {tries}]")
-        stats()
+        play_again = input("Would you like to play again? (y/n): ") # "play again?" prompt
+        if play_again.lower() == 'y':
+            tries = 0 
+            solution = random.randint(1,10)
+            print("""
+            ***** Ready, Set, Go! *****""")
+            print("solution = ", solution) # DELETE THIS LINE
+            continue
+        else:
+            print(f"""
+            Until next time, Goodbye!
+            """)
+            stats()
+            break
 
 # Lastly, calling the code to run in this order:
 menu()
 while True:
-    user_choice = input("What would you like to do? -> ")
+    user_choice = input("WHAT DO YOU WANT TO DO? > ")
     if user_choice.lower() == 'exit':
         break
     elif user_choice.lower() == 'help':
@@ -87,8 +96,5 @@ while True:
     elif user_choice.lower() == 'play':
         start_game()
         break
-
-
-# Trying to work out some error issues: 
-# 1) Why are some of my errors are passive? For example: if I guess an integer within range on my first attempt everything works fine. But say I guess an out-of-range integer for my second attempt, it will not raise an error (FYI: if i guess out-of-range on my first attempt, it WILL raise an error). I'm sure there is some issue in my order of things.
-# 2) my raised errors will restart the game, re-randomizing the number. How do I put it so that it continues to let the user try again within the current game?
+        
+# DON'T FORGET: Delete lines revealing the solution (line 49, line 83)
